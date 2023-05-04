74e3f4c3fc6ce968e79f42cc35578f8d04b36d2a
coarf <coarf@hotmail.com>
2023-05-04 12:47:22 -0500

    creada la clase persona e instancia en la clase app

--- src/App.java
+++ src/App.java
@@ -1,5 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Persona person1 = new Persona(2L,"alvaro","rodriguez", "fernandez", 35);
+        System.out.println(person1.getName());
     }
 }
