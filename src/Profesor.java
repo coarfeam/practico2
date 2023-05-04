219108b557ed9e3a62456b9bc3b41e4bdf3a7948
coarf <coarf@hotmail.com>
2023-05-04 13:09:51 -0500

    agregada la clase profesor con sus atributos

---
+++ src/Profesor.java
@@ -0,0 +1,26 @@
+public class Profesor extends Persona{
+    private String materia;
+
+    public String getMateria() {
+        return materia;
+    }
+
+    public void setMateria(String materia) {
+        this.materia = materia;
+    }
+
+    public Profesor(String materia) {
+        this.materia = materia;
+    }
+
+    public Profesor(Long cedula, String name, String apellido1, String apellido2, int edad, String materia) {
+        super(cedula, name, apellido1, apellido2, edad);
+        this.materia = materia;
+    }
+
+    
+    public String calificar(int nota){
+        return "su nota es: "+nota;
+    }
+    
+}
